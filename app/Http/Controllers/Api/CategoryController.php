fdf2cf03aa29055d80d58ca71fafa5950ad31738
Ryann Mesquita <ryann.araujo@gmail.com>
2024-01-18 16:33:37 -0300

    Create Model and Migration Company and Category

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateCategory;
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class CategoryController extends Controller
+{
+    protected $repository;
+
+    public function __construct( Category $model)
+    {
+        $this->repository = $model;
+    }
+
+    public function index()
+    {
+        $categories = $this->repository->get();
+        return CategoryResource::collection($categories);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreUpdateCategory $request)
+    {
+       $data = $request->validated();
+       $data['url'] = Str::slug($data['title'], '-') ;
+       $category = $this->repository->create($data);
+
+       return new CategoryResource($category);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($url)
+    {
+        $category = $this->repository->where('url', $url)->firstOrFail();
+        return new CategoryResource($category);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(StoreUpdateCategory $request, $url)
+    {
+        $category = $this->repository->where('url', $url)->firstOrFail();
+
+        $category->update($request->validated());
+
+        return response()->json(['message' => 'success']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($url)
+    {
+        $category = $this->repository->where('url', $url)->firstOrFail();
+
+        $category->delete();
+
+        return response()->json([], 204);
+    }
+}
